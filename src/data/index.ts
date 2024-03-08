739a8497790699098f1260b8dbc8f5eb9be1e336
yoshikouki <yoshikouki@gmail.com>
2024-03-08 10:04:38 +0900

    Validate and type JSON

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,2 +1,45 @@
+import { z } from "zod";
+
 import rawJSON from "@/data/data.json";
-export const data = rawJSON;
+
+export const Location = z.object({
+  name: z.string(),
+  address: z.string(),
+});
+
+export const Segment = z.object({
+  id: z.number(),
+  name: z.string(),
+  start_time: z.coerce.date(),
+  end_time: z.coerce.date(),
+});
+
+export const Supporter = z.object({
+  id: z.number(),
+  name: z.string(),
+  url: z.string().url(),
+});
+
+export const Event = z.object({
+  id: z.number(),
+  name: z.string(),
+  url: z.string().url(),
+  start_time: z.coerce.date(),
+  end_time: z.coerce.date(),
+  max_participants: z.number(),
+  participants_count: z.number(),
+  location: Location,
+  segments: z.array(Segment),
+  supporters: z.array(z.number()),
+});
+
+export const Data = z.object({
+  events: z.record(Event),
+  supporters: z.record(Supporter),
+});
+
+export type Data = {
+  events: Record<number, Event>;
+};
+
+export const data = Data.parse(rawJSON);
