00eacc40ba978892da2ad341de84a94e39059cfe
yoshikouki <yoshikouki@gmail.com>
2024-03-09 22:58:11 +0900

    Fix style that collapses on scrolling

--- src/features/app/index.tsx
+++ src/features/app/index.tsx
@@ -3,7 +3,7 @@ import { EventList } from "./event-list";
 export const App = () => {
   return (
     <div className="flex flex-col gap-8 w-full">
-      <div className="min-h-dvh flex flex-col justify-center items-center p-4 gap-4">
+      <div className="min-h-svh flex flex-col justify-center items-center p-4 gap-4">
         <h2 className="font-black text-5xl text-primary">Engineer Bar</h2>
         <p>
           <span className="font-bold bg-primary text-primary-foreground px-1 mr-1">
