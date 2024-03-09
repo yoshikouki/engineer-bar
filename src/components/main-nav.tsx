e751aaec59a137cf51f956d4217715c9f37d0051
yoshikouki <yoshikouki@gmail.com>
2024-03-09 21:43:02 +0900

    Switch main nav depending on the routing

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,22 +1,33 @@
 import { Maximize, Minimize } from "lucide-react";
+import { useEffect, useState } from "react";
 
 import { Button } from "./ui/button";
 
 export const MainNav = () => {
+  const [currentPath, setCurrentPath] = useState("");
+
+  useEffect(() => {
+    setCurrentPath(window.location.pathname);
+  }, []);
+
   return (
-    <nav className="fixed bottom-0 z-10 max-w-md w-screen flex justify-between py-8 pointer-events-none">
-      <Button className="pointer-events-auto px-4 py-8" variant="ghost">
-        <span className="mr-2">
-          <Maximize />
-        </span>
-        Global
-      </Button>
-      <Button className="pointer-events-auto px-4 py-8" variant="ghost">
-        <span className="mr-2">
-          <Minimize />
-        </span>
-        Local
-      </Button>
-    </nav>
+    <>
+      {currentPath.startsWith("/app") && (
+        <nav className="fixed bottom-0 z-10 max-w-md w-screen flex justify-between py-8 pointer-events-none">
+          <Button className="pointer-events-auto px-4 py-8" variant="ghost">
+            <span className="mr-2">
+              <Maximize />
+            </span>
+            Global
+          </Button>
+          <Button className="pointer-events-auto px-4 py-8" variant="ghost">
+            <span className="mr-2">
+              <Minimize />
+            </span>
+            Local
+          </Button>
+        </nav>
+      )}
+    </>
   );
 };
