d8d607e1138e53d14b682091987c955f2a8790cc
yoshikouki <yoshikouki@gmail.com>
2024-03-12 17:28:27 +0900

    Clean code

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import { reactRenderer } from "@hono/react-renderer";
 import { Hono } from "hono";
-import { createBunWebSocket, serveStatic } from "hono/bun";
+import { serveStatic } from "hono/bun";
 import { logger } from "hono/logger";
 import { secureHeaders } from "hono/secure-headers";
 
-import { ServerWebSocket } from "bun";
-import { WSContext } from "hono/ws";
+import { Server, ServerWebSocket } from "bun";
 import { App } from "./features/app";
 import { Lobby } from "./features/lobby";
 
 const app = new Hono();
-
-const { upgradeWebSocket } = createBunWebSocket();
 
 app.use(secureHeaders());
 app.use(logger());
@@ -31,7 +28,7 @@ app.get(
             bootstrapModules: ["/src/client.tsx"],
           }),
     },
-  }),
+  })
 );
 
 const routes = app
@@ -42,63 +39,47 @@ const routes = app
     const eventId = parseInt(c.req.param("eventId"), 10);
     return c.render(<Lobby eventId={eventId} />);
   })
-  .get(
-    "/ws",
-    upgradeWebSocket((c) => {
-      const connections: WSContext[] = [];
-      return {
-        onOpen(event, ws) {
-          connections.push(ws);
-          console.log("onOpen: connections:", connections.length);
-          console.log("connected");
-        },
-        onMessage(event, ws) {
-          console.log("onMessage: connections:", connections.length);
-          for (const conn of connections) {
-            if (conn !== ws) {
-              conn.send(
-                JSON.stringify({
-                  id: crypto.randomUUID(),
-                  content: event.data,
-                }),
-              );
-            }
-          }
-        },
-        onClose() {
-          console.log("disconnected");
-        },
-      };
-    }),
-  );
+  .get("/ws", async (c, next) => {
+    const server = c.env as unknown as Server;
+    const upgradeResult = server.upgrade(c.req.raw, {
+      data: {},
+    });
+    if (upgradeResult) {
+      return new Response(null);
+    }
+    await next(); // Failed
+  });
 
 const port = process.env.PORT || "8888";
 console.log(`Listening on http://localhost:${port}`);
 
 export type AppType = typeof routes;
 
+type WebSocketData = Record<string, never>;
+
 const server = Bun.serve({
   port,
   fetch: app.fetch,
   websocket: {
-    open: (ws: ServerWebSocket) => {
+    open: (ws: ServerWebSocket<WebSocketData>) => {
+      console.log("ws.data:", ws.data);
       ws.subscribe("robby");
       console.log("WebSocket is connected.");
     },
     message: (
-      ws: ServerWebSocket,
-      message: string | ArrayBuffer | Uint8Array,
+      ws: ServerWebSocket<WebSocketData>,
+      message: string | ArrayBuffer | Uint8Array
     ) => {
       server.publish(
         "robby",
         JSON.stringify({
-          id: "server",
+          id: crypto.randomUUID(),
           content: message,
           user: { id: "server" },
-        }),
+        })
       );
     },
-    close: (ws: ServerWebSocket) => {
+    close: (ws: ServerWebSocket<WebSocketData>) => {
       ws.unsubscribe("robby");
       console.log("WebSocket is closed.");
     },
